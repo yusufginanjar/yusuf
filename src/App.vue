e6851bd105769227bced58a0e7365efb9ba49979
yusufginanjar <yusufginanjar16@gmail.com>
2022-09-14 23:03:21 +0700

    Dev-vue: Pages init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+import Skills from './pages/Skills.vue'
+import Portfolio from './pages/Portfolio.vue'
+import Contact from './pages/Contact.vue'
 </script>
 
 <template>
 
   <main>
-    <Home />
+    <Contact />
   </main>
 </template>
 
