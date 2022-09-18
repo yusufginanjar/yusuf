f727fb0c2a8d85660f6357a5017bf52b55cb7d5d
yusufginanjar <yusufginanjar16@gmail.com>
2022-09-18 17:49:05 +0700

    Vue navbar component

---
+++ src/Personal.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+    <main>
+    <div class="w-full min-h-screen px-2 py-12 bg-[url('assets/images/bg2.jpg')] bg-cover">
+        <div class="w-full flex justify-center">
+            <div class="max-w-md relative">
+                <router-view/>
+                <Navbar/>
+            </div>
+        </div>  
+    </div>
+    </main>
+</template>
+
+<script>
+    import Navbar from './components/Navbar.vue'
+
+    export default {
+        components: {
+            Navbar,
+        },
+    }
+</script>
+
+
+    
+<style scoped>
+</style>
+    
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+import Portfolio from './pages/Portfolio.vue'
+import Skills from './pages/Skills.vue'
+import Contact from './pages/Contact.vue'
+// import NotFound from './pages/NotFound.vue'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/portfolio', component: Portfolio },
+    { path: '/skills', component: Skills },
+    { path: '/contact', component: Contact },
+    // { path: '/:notFound(.*)', component: NotFound }
+]
+
+const app = createApp(App)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+app.use(router)
+
+app.mount('#app')
